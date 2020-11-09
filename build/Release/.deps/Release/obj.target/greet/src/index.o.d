cd79599035ffc854af49c073d7c01f2e5239b94b
Andy <andypuneri@gmail.com>
2020-11-09 02:28:14 -0800

    hello Mike example c++

---
+++ build/Release/.deps/Release/obj.target/greet/src/index.o.d
@@ -0,0 +1,15 @@
+cmd_Release/obj.target/greet/src/index.o := g++ -o Release/obj.target/greet/src/index.o ../src/index.cpp '-DNODE_GYP_MODULE_NAME=greet' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DNAPI_DISABLE_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/home/andy/.cache/node-gyp/10.13.0/include/node -I/home/andy/.cache/node-gyp/10.13.0/src -I/home/andy/.cache/node-gyp/10.13.0/deps/openssl/config -I/home/andy/.cache/node-gyp/10.13.0/deps/openssl/openssl/include -I/home/andy/.cache/node-gyp/10.13.0/deps/uv/include -I/home/andy/.cache/node-gyp/10.13.0/deps/zlib -I/home/andy/.cache/node-gyp/10.13.0/deps/v8/include -I/home/andy/andyGit/nodejs-c-tests/node_modules/node-addon-api  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/greet/src/index.o.d.raw   -c
+Release/obj.target/greet/src/index.o: ../src/index.cpp \
+ /home/andy/andyGit/nodejs-c-tests/node_modules/node-addon-api/napi.h \
+ /home/andy/.cache/node-gyp/10.13.0/include/node/node_api.h \
+ /home/andy/.cache/node-gyp/10.13.0/include/node/node_api_types.h \
+ /home/andy/andyGit/nodejs-c-tests/node_modules/node-addon-api/napi-inl.h \
+ /home/andy/andyGit/nodejs-c-tests/node_modules/node-addon-api/napi-inl.deprecated.h \
+ ../src/greeting.h
+../src/index.cpp:
+/home/andy/andyGit/nodejs-c-tests/node_modules/node-addon-api/napi.h:
+/home/andy/.cache/node-gyp/10.13.0/include/node/node_api.h:
+/home/andy/.cache/node-gyp/10.13.0/include/node/node_api_types.h:
+/home/andy/andyGit/nodejs-c-tests/node_modules/node-addon-api/napi-inl.h:
+/home/andy/andyGit/nodejs-c-tests/node_modules/node-addon-api/napi-inl.deprecated.h:
+../src/greeting.h:
