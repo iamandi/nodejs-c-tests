bcde8c4ff094df85fe6924fed48df5603b84085a
iamandi <aakawade9@gmail.com>
2020-11-09 15:22:01 -0800

    change ffi to ffi-napi

--- hello_mike_c_ffi/greet.js
+++ hello_mike_c_ffi/greet.js
@@ -1,14 +1,16 @@
-var ffi = require('ffi')
+var ffi = require("ffi-napi");
 
-var libgreet = ffi.Library('./libgreet', {
-  'greet': [ 'string', [ 'string' ] ]
-})
+var libgreet = ffi.Library("./libgreet", {
+  greet: ["string", ["string"]],
+});
 
 if (process.argv.length < 3) {
-  console.log('Arguments: ' + process.argv[0] + ' ' + process.argv[1] + ' <max>')
-  process.exit()
+  console.log(
+    "Arguments: " + process.argv[0] + " " + process.argv[1] + " <max>"
+  );
+  process.exit();
 }
 
-var output = libgreet.greet(process.argv[2])
+var output = libgreet.greet(process.argv[2]);
 
-console.log('Your output: ' + output)
+console.log("Your output: " + output);
